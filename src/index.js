584c44140bafb62e6ad54bac939e6cd607a068fd
Rahul <rahuls@live.ca>
2021-06-08 13:40:15 +0530

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from "./redux/Store"
-import { BrowserRouter as Router } from "react-router-dom";
 
 
 
